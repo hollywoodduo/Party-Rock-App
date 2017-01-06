2e3b2c0a4da66e9f202d3aaf5dac05e9b0ea7dab
hollywoodduo <matthew.perkins@gmail.com>
2017-01-05 19:59:36 -0500

    initial commit

---
+++ Party Rock App/Party Rock App/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Party Rock App
+//
+//  Created by Matthew J. Perkins on 1/5/17.
+//  Copyright © 2017 Matthew J. Perkins. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
