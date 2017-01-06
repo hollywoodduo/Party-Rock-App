721759a2f50fccdaa31de7b9aebc1018173a5ca1
hollywoodduo <matthew.perkins@gmail.com>
2017-01-06 15:04:39 -0500

    final version before AWK conversion

--- Party Rock App/Party Rock App/VideoVC.swift
+++ Party Rock App/Party Rock App/VideoVC.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class VideoVC: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+        return _partyRock
+        } set {
+        _partyRock = newValue
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleLabel.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
     }
 
     override func didReceiveMemoryWarning() {
